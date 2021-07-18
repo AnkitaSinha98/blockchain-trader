ace36a5ae8a2583c61dbc91baaa7f4745e15f5ec
Soumadeep Dhar <nicksouma@gmail.com>
2021-07-18 11:24:25 +0530

    Added the first level UI

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: "Farm",
+    description:
+      "The farming systems that significantly contribute to the agriculture of India are subsistence farming, organic farming, industrial farming.",
+    imageUrl: process.env.PUBLIC_URL + "/assets/farm1.jpeg",
+    time: 1500,
+  },
+  {
+    title: "Equipments",
+    description:
+      "Co-operative farming refers to the pooling of farming resources such as fertilizers, pesticides, farming equipment such as tractors. ",
+    imageUrl: process.env.PUBLIC_URL + "/assets/farm2.jpg",
+    time: 1500,
+  },
+];
+
+export default places;
